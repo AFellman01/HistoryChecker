b8a49d96214df59bd73b7b702ab3b9d7a5b60632
Le Vu Hoang <rainyhoang@gmail.com>
2017-12-13 12:50:13 -0500

    model and passport

---
+++ models/user.js
@@ -0,0 +1,35 @@
+var bcrypt = require("bcrypt-nodejs");
+module.exports = function (sequelize, DataTypes) {
+	var Admin = sequelize.define("Admin", {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+        },
+
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+      validate: {
+        isEmail: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    name: {
+    	type: DataTypes.STRING,
+    	allowNull:false,
+    	len: [1]  
+
+    },
+
+    DateOfBirth: {
+    	type: DataTypes.STRING,
+    	allowNull: false
+    	len:[1]
+    }
+  });
+}
