cd869cdf609581a6f63a453628f407d813aceb9d
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-12-14 12:53:34 -0500

    syntax

--- models/blog.js
+++ models/blog.js
@@ -34,7 +34,7 @@ module.exports = function (sequelize, DataTypes) {
       type:DataTypes.BOOLEAN,
       allowNull: false,
       defaultValue: false,
-    }
+    });
 
       return POST;
 }
--- models/credential.js
+++ models/credential.js
@@ -10,6 +10,6 @@ module.exports = function (sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false,
       unique: true,
-}
+});
       return Credential;
 }
