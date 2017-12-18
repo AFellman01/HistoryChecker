0d3ff4ba32d28de9523b8820cfb79437f1488e71
Sean Clark <seanclark@Seans-MacBook-Pro.local>
2017-12-18 11:50:16 -0500

    config.json back and functioning like a playa

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ var mysql = require("mysql");
 var connection = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "Tiger1932",
+  password: "",
   database: "historyDB"
 });
 
