46819a3d285915f92bdb80b1d40da9abca546233
Alexander Fellman <alexanderfellman@Alexanders-MacBook-Pro.local>
2017-12-19 11:09:11 -0500

    process

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var db = require("./models");
 var app = express();
-var PORT = 8080;
+var PORT = process.env.PORT || 8080;
 //var router = express.Router();
 // Sets up the Express app to handle data parsing
 
